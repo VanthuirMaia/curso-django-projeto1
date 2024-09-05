0b08889d45ba1cb9c74fab91904e80aec641cdb8
Bowie <vanmaiasf@gmail.com>
2024-09-05 00:00:23 -0300

    Adicionando apps

--- projeto/urls.py
+++ projeto/urls.py
@@ -22,4 +22,6 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'home.html')
 
 def contato(request):
     return HttpResponse('Contato')
