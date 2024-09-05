589804067b2b9d015831e573c30a4d49021a40af
Bowie <vanmaiasf@gmail.com>
2024-09-04 23:16:22 -0300

    Criando views e URLs

---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse('HOME')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
