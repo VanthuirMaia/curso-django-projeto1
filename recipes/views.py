0326fe105170b54ecddbc7b77f46c55c2c5734b7
Bowie <vanmaiasf@gmail.com>
2024-09-08 15:44:17 -0300

    Adicionado Partials e Pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Vanthuir Maia',
+    })
