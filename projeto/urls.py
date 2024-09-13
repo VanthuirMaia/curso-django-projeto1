716ee41c070d3e02c58f4f1d592b3e1ffb9df065
Bowie <vanmaiasf@gmail.com>
2024-09-12 23:38:03 -0300

    Adicionando categorias e receitas pelo ADMIN

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')), # Inclui as Urls do app RECIPES
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
